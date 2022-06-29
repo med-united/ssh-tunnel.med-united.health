8e93f51798ab475a4638cfc08fcacea124501c27
simoneS96 <simone.stifano@incentergy.de>
2022-06-29 16:51:01 +0200

    initialize repository

---
+++ src/test/java/health/medunited/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package health.medunited;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
