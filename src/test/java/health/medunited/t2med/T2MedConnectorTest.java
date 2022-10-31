b0a0c54c9b3f966e5edd6a8cccf97f31e665c5bf
simoneS96 <simone.stifano@incentergy.de>
2022-10-31 17:04:36 +0100

    delete unused header factory

--- src/test/java/health/medunited/t2med/T2MedConnectorTest.java
+++ src/test/java/health/medunited/t2med/T2MedConnectorTest.java
@@ -2,8 +2,6 @@ package health.medunited.t2med;
 
 import javax.inject.Inject;
 
-import health.medunited.client.AuthorizationHeaderFactory;
-import io.quarkus.test.Mock;
 import org.hl7.fhir.r4.model.Bundle;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
